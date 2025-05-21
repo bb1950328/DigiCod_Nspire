028e02cd5ad8cbae1d834bdba839edd41dc0f6f0
bb1950328 <bb1950328@gmail.com>
2025-05-21 08:57:29 +0200

    add unit tests

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python scripts for the DigiCod module
+to run on the TI-Nspire calculator
+
---
+++ test.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def _get_test_functions():
+    return [obj for name, obj in globals().items() if callable(obj) and name.startswith("test_")]
+
+def test_a():
+    print("test_a")
+
+def test_b():
+    print("test_b")
+    raise ValueError("test_b")
+
+
+if __name__ == '__main__':
+    fail = False
+    for func in _get_test_functions():
+        try:
+            func()
+        except Exception as e:
+            sys.print_exception(e)
+            fail = True
+    if fail:
+        sys.exit(1)
